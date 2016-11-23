3f3c307292de5b2d4f17da8c400b957f730edc89
[ajaz syed] <[ajazsyed@gmail.com]>
2016-11-22 18:54:59 -0800

    hash replacement exercise

---
+++ nove22.py
@@ -0,0 +1,20 @@
+''' take the users input and replace all of the characters with the # except
+for the last 4 characters
+
+
+pseudo code:
+use while loop:
+replace word[0],[1],[3],[4] with #'''
+
+word = (input("give me a word greater than four letters"))
+
+wordlength = len(word)
+
+if wordlength <=4:
+            newword = "#" * wordlength
+
+else:
+            lastfour = word[wordlength -4:wordlength]
+            hashtags = "#" * (wordlength -4)
+            newword = hashtags + lastfour
+print(newword)
